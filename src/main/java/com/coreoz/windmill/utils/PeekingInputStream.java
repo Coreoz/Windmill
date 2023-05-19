58e0e3ee7a30bf9bfc61794e83ef25840bcb6247
Benoit Vasseur <bvasseur@coreoz.com>
2023-05-19 11:03:32 +0200

    Add a utility to check if the first few bytes of a file are a bom

---
+++ src/main/java/com/coreoz/windmill/utils/PeekingInputStream.java
@@ -0,0 +1,35 @@
+package com.coreoz.windmill.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PushbackInputStream;
+
+/**
+ * An InputStream wrapper that provides a peeking feature.
+ * See {@link BomCharset} for usage
+ */
+public class PeekingInputStream {
+    // This number comes from the max possible length for available boms
+    private static final int MAX_BOM_LENGTH = BomCharset.maxBomLength();
+
+    private final PushbackInputStream fileStream;
+
+    public PeekingInputStream(InputStream fileStream) {
+        this.fileStream = new PushbackInputStream(fileStream, MAX_BOM_LENGTH);
+    }
+
+    public byte[] peekMaxBomLength() throws IOException {
+        return peek(MAX_BOM_LENGTH);
+    }
+
+    private byte[] peek(int bytesToRead) throws IOException {
+        byte[] bytesPeeked = new byte[]{};
+        fileStream.read(bytesPeeked, 0, bytesToRead);
+        this.fileStream.unread(bytesPeeked);
+        return bytesPeeked;
+    }
+
+    public PushbackInputStream peekedStream() {
+        return this.fileStream;
+    }
+}
