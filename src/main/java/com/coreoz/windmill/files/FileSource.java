09f738666541183f1daba41899a84107900dd4bd
Aurélien Manteaux <amanteaux@coreoz.com>
2023-05-24 11:30:34 +0200

    Remove PeekingInputStream in favor of FileSource

--- src/main/java/com/coreoz/windmill/files/FileSource.java
+++ src/main/java/com/coreoz/windmill/files/FileSource.java
@@ -1,29 +1,30 @@
 package com.coreoz.windmill.files;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-
 import lombok.SneakyThrows;
 
-public class FileSource {
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.PushbackInputStream;
 
-	private final BufferedInputStream inputStream;
+public class FileSource {
+	private final PushbackInputStream bufferedStream;
 
 	private FileSource(InputStream inputStream) {
-		this.inputStream = new BufferedInputStream(inputStream);
+		this.bufferedStream = new PushbackInputStream(
+			inputStream,
+			Math.max(BomCharset.maxBomLength(), FileTypeGuesser.maxSignatureLength())
+		);
 	}
 
 	public InputStream toInputStream() {
-		return inputStream;
+		return bufferedStream;
 	}
 
 	@SneakyThrows
-	public byte[] peek(int nbBytesToRead) {
-		inputStream.mark(nbBytesToRead);
-		byte[] peeked = inputStream.readNBytes(nbBytesToRead);
-		inputStream.reset();
-		return peeked;
+	public byte[] peek(int bytesToRead) {
+		byte[] bytesPeeked = bufferedStream.readNBytes(bytesToRead);
+		bufferedStream.unread(bytesPeeked);
+		return bytesPeeked;
 	}
 
 	public static FileSource of(InputStream inputStream) {
@@ -33,5 +34,4 @@ public class FileSource {
 	public static FileSource of(byte[] inputData) {
 		return of(new ByteArrayInputStream(inputData));
 	}
-
 }
--- src/main/java/com/coreoz/windmill/files/FileTypeGuesser.java
+++ src/main/java/com/coreoz/windmill/files/FileTypeGuesser.java
@@ -11,6 +11,12 @@ public final class FileTypeGuesser {
 	private static final byte[] ZIP_FIRST_BYTES = {0x50, 0x4B};
 	private static final byte[] XLS_FIRST_BYTES = fromIntArray(new int[]{ 0xD0, 0xCF, 0x11, 0xE0, 0xA1, 0xB1, 0x1A, 0xE1 });
 
+	private static final int MAX_SIGNATURE_LENGTH = Math.max(ZIP_FIRST_BYTES.length, XLS_FIRST_BYTES.length);
+
+	public static int maxSignatureLength() {
+		return MAX_SIGNATURE_LENGTH;
+	}
+
 	/**
 	 * Guess the type of a file by reading the first bytes of the file
 	 */
@@ -26,8 +32,8 @@ public final class FileTypeGuesser {
 
 	private static byte[] fromIntArray(int[] values) {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		for (int i = 0; i < values.length; ++i) {
-			baos.write(values[i]);
+		for (int value : values) {
+			baos.write(value);
 		}
 
 		return baos.toByteArray();
